f2ac1eb362fc2513fd1c59925dab94dcd933bae5
Mukes Kumar <156907997+mukesh2002-dev@users.noreply.github.com>
2026-06-19 04:32:10 +0530

    Create test_calculator.py

---
+++ test/test_calculator.py
@@ -0,0 +1,14 @@
+from app.calculato import add ,substract
+
+'''
+from app -- folder
+calculator --> file name
+import add , substracts (function)
+'''
+
+def test_Add():
+   assert add(2,3) == 5
+
+
+def test_substract():
+assert substract(5,3) == 2
